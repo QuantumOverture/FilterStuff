6b61d49323d5ae03c7b45c877316f17eef963498
ismaildude <38566647+ismaildude@users.noreply.github.com>
2021-07-10 17:44:48 +0500

    Add files via upload

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 urlpatterns = [
+    path('register/',views.RegisterView,name="RegisterView"),
+    path('login/',auth_views.LoginView.as_view(template_name="MainApp/login.html"),name="LoginView"),
+    path('logout/',auth_views.LogoutView.as_view(template_name="MainApp/logout.html"),name="LogoutView"),
     path('u/<str:UserName>',views.UserView,name="UserView"),
+    path('u/',views.UserViewRedirect,name="UserViewRedirect"),
     path('',views.HomePage,name="HomePage"),
     
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
+from .forms import UserRegisterForm
+from django import forms
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 def HomePage(request):
-    return HttpResponse("<h1>HomePage</h1>")
-    
+	return render(request,"MainApp/index.html")
+
+@login_required    
 def UserView(request,UserName):
+
+    if UserName != request.user.username:
+        return redirect("UserView",UserName = request.user.username)
     return HttpResponse("<h1>{}</h1>".format(UserName))
+    
+@login_required 
+def UserViewRedirect(request):
+    return redirect("UserView",UserName = request.user.username)
+
+def RegisterView(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('LoginView')
+    else:
+        form = UserRegisterForm()
+    return render(request,"MainApp/register.html",{'form':form})
